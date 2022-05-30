7e25f4e6d5f958f2a56647c1295421b096965503
Torsten Kunkel <torsten.kunkel@volkswagen.de>
2022-05-30 10:35:46 +0200

    * theming

--- src/app/services/theme-decider.service.ts
+++ src/app/services/theme-decider.service.ts
@@ -5,19 +5,19 @@ import {Injectable} from '@angular/core';
 })
 
 export class ThemeDeciderService {
-
   constructor() {
+    this.myAppLogo = 'nadin-logo.gif';
+    // this.myAppLogo = 'ldb.png';
   }
 
-  private myAppLogoFinished: string;
   private myAppLogo: string;
+
+  set appLogo(value: string) {
+    this.myAppLogo = value;
+  }
 
   get appLogo(): string {
     return this.myAppLogo;
-  }
-
-  get appLogoFinished(): string {
-    return this.myAppLogoFinished;
   }
 
   private myTheme: string;
@@ -37,48 +37,18 @@ export class ThemeDeciderService {
     if (!newTheme) {
       return;
     }
-
-    if ((newTheme === 'Easter')
-      || (newTheme === 'Spring')
-      || (newTheme === 'Summer')
-      || (newTheme === 'Winter')) {
+    if (newTheme === 'Winter') {
+      this.myTheme = newTheme;
+    } else if (newTheme === 'Easter') {
+      this.myTheme = newTheme;
+    } else if (newTheme === 'Spring') {
+      this.myTheme = newTheme;
+    } else if (newTheme === 'Summer') {
       this.myTheme = newTheme;
     } else {
       console.log(`Invalid theme '${newTheme}' ignoring.`);
     }
-  }
-
-  set application(value: string) {
-    this._application = value;
-    switch (this.application.toLowerCase()) {
-      case 'nadin':
-        this.myAppLogo = 'nadin-running.gif';
-        this.myAppLogoFinished = 'nadin-done.png';
-        break;
-      case 'ldb':
-        this.myAppLogo = 'ldb-running.png';
-        this.myAppLogoFinished = 'ldb-done.png';
-        break;
-      case 'vw':
-        this.myAppLogo = 'vw.png';
-        this.myAppLogoFinished = 'vw.png';
-        break;
-      default:
-        console.error(`Unknown application ${this.application}`);
-        break;
-    }
-    console.log(this.application.toLowerCase());
-    console.log(this.myAppLogo);
-  }
-
-  private _application: string;
-
-
-  get application(): string {
-    if (!this._application) {
-      this._application = 'Nadin';
-    }
-    return this._application;
+    console.log(`Using theme '${this.myTheme}'.`);
   }
 
   private generateRandomTheme(): string {
@@ -88,54 +58,9 @@ export class ThemeDeciderService {
       return 'Easter';
     } else if (now.getMonth() >= 11 || now.getMonth() <= 0) {
       return 'Winter';
-    } else if (now.getMonth() >= 4 && now.getMonth() <= 9) {
+    } else if (now.getMonth() >= 4 || now.getMonth() <= 9) {
       return 'Summer';
     }
     return 'Spring';
-  }
-
-  get possibleFakeStatusses(): string[] {
-    switch (this.application.toLowerCase()) {
-      case 'nadin':
-        return [
-          'Granting admin permissions to developers',
-          'Upgrading servers to NADIN 18.6',
-          'Dropping databases from backups',
-          'Answering emails with Lorem Ipsum generator',
-          'Setting customer names to phonebook of London',
-          'Randomizing prices with e^3*y+42€',
-          'Downloading and printing cloud',
-          'Schedule MS Teams meeting with colleagues',
-          'Plan new bugs for next release',
-          'Ordering pizza for next break',
-          'Feeding coffee to coding monkeys',
-          'Deploying new version announced tomorrow',
-          'Cloud performance might be reduced due to fast winds …',
-          'Some servers in the cloud are unavaiable due to heavy storms …',
-          'Removing caches to save storage',
-          'Splitting list to prepare for two parallel streams',
-          'The NADIN Modules lived together in harmony but everything changed when the Fleet Agency Business attacked',
-          'Never argue with the data',
-          'Debugging you must',
-          'Nothing shocks me – I fix bugs in NADIN',
-          'One NADIN to rule them all',
-          'Data cloud is full, please switch to local data storage solutions',
-        ];
-      case 'ldb':
-        return [
-          'Removing all users from production',
-          'Filing bugs from Lorem ipsum generator',
-          'Randomizing planned values',
-          'Ordering pizza for developers',
-          'Cooking coffee for next meeting',
-          'Adding new random name generator to user management',
-          'Throwing away permissions',
-          'Resetting test environment',
-          'Uploading new release 2.0 to production',
-        ];
-      default:
-        console.error(`Unknown application ${this.application}`);
-        break;
-    }
   }
 }
